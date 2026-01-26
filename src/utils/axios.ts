2e20427a849eead52138ee9127e95e0d57e7160d
Mustarphy <abdulazeezmustapha204@gmail.com>
2026-01-26 08:28:05 -0800

    committing admin payment panel

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -5,9 +5,9 @@ const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL
   ? import.meta.env.VITE_API_URL + "/api"
   : "http://localhost:8000/api",
-  withCredentials: false, // ❌ don't send cookies
+  withCredentials: false, // don't send cookies
   headers: {
-    "x-api-key": "secret123", // 🔹 API key for admin middleware
+    "x-api-key": "secret123", // API key for admin middleware
   },
 });
 
